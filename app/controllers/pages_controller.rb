917f38e6894998ca5b71e79ddfefbf7e58d4f43a
streetspyders <streetspyders@aol.com>
2024-06-14 15:31:39 -0400

    addedd pagination to index page

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,5 +1,7 @@
 class PagesController < ApplicationController
+    ALERTS_PER_PAGE = 25
   def index
-    @alerts = Alert.includes(:member).all
+    @page = params.fetch(:page,0).to_i
+    @alerts = Alert.includes(:member).offset(@page*ALERTS_PER_PAGE).limit(ALERTS_PER_PAGE)
   end
 end
